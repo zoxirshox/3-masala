68a61ccb1bce3269a51cde3bad0f00258edf7776
Zoxirshox <@mrcewa2@gmail.com>
2023-02-25 23:49:27 +0500

    3-masala

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var a = +prompt("son yozing");
+var elHeading = document.querySelector("h2")
+
+if (a % 2 == 0) {
+    document.getElementById('travel').textContent = "juft"
+} else {
+    document.getElementById('travel').textContent = "toq"
+}
